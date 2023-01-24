03cb7162e1072e25eff6a248d3d2bddee77d09af
codeekta28 <codeekta28@gmail.com>
2023-01-24 19:41:51 +0530

    changed typescript to normal js with sign in sign up page

--- pages/_app.js
+++ pages/_app.js
@@ -4,11 +4,10 @@ import { Provider } from "react-redux";
 import store from "../store/store";
 import "../styles/globals.css";
 import { useEffect } from "react";
-import type { AppProps } from "next/app";
 import { RedirectUserOnTokenBasis } from "../helper/RedirectUserOnTokenBasis";
 import { useRouter } from "next/router";
 
-export default function App({ Component, pageProps }: AppProps) {
+export default function App({ Component, pageProps }) {
   const router = useRouter();
   useEffect(() => {
     require("bootstrap/dist/js/bootstrap.bundle.min.js");
